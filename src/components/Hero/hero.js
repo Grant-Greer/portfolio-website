00b3b7e71ce38d7d124623d2b63ec7ac1da24e70
Grant Greer <viridiang93@gmail.com>
2022-06-01 06:54:51 -0700

    updated name

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -13,7 +13,7 @@ export default function hero() {
   return (
     <header>
       <h1>
-        Hi, I'm Travis Pandos is great.<br />
+        Hi, I'm Ronald McdDonald <br />
         <span> Full Stack Software Developer</span>
       </h1>
 
